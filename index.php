8968e437caef9bc23593c1bb94fb83bac734c6df
Matt Smith <mattsmith@Matts-MacBook-Pro.local>
2019-06-26 12:43:02 -0400

    Inital Commit

---
+++ index.php
@@ -0,0 +1,96 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Rubik+Mono+One&display=swap" rel="stylesheet">
+
+    <style type="text/css">
+    	i.fas,i.fab {
+    		color: #4267b2;
+    	}
+    	.footer {
+    		bottom: 10px;
+    	}
+    	.main {
+    	}
+    	.mshadow {
+    		box-shadow: 10px 10px 5px grey;
+    	}
+    </style>
+
+    <title>High Country Hardscapes and Landscape Design - Upstate NY Hardscaping Professionals</title>
+  </head>
+  <body>
+    <div class="container">
+      <!-- Content here -->
+      <div class="row header  align-items-center">
+      	<div class="col-md-3 mb-4">
+      		<div class="mshadow">
+	      		<a href="./index.html">
+	      			<img src="./img/hchld.logo.jpg" style="width:250px;">
+	      		</a>
+	      	</div>
+      	</div>
+      	<div class="col-md-9 text-center">
+  			<h2 class="title">High Country Hardscapes and Landscape Design Website is Under Construction</h2>
+  			<p>Just like any of our great stonework, concrete or pavers jobs this masterpeice will start with some dirty work. When its complete it will be the place to reference everything we do.</p>	
+      	</div>
+      </div>
+      <div class="row main mt-4">
+      	<div class="col-md">
+
+      		<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+      		  <div class="carousel-inner">
+      		    <div class="carousel-item active">
+      		      <img src="img/const1.jpg" class="d-block w-100" alt="">
+      		    </div>
+      		    <div class="carousel-item">
+      		      <img src="img/const2.jpg" class="d-block w-100" alt="...">
+      		    </div>
+      		    <div class="carousel-item">
+      		      <img src="img/const3.jpg" class="d-block w-100" alt="...">
+      		    </div>
+      		    <div class="carousel-item">
+      		      <img src="img/const4.jpg" class="d-block w-100" alt="...">
+      		    </div>
+      		    <div class="carousel-item">
+      		      <img src="img/const5.jpg" class="d-block w-100" alt="...">
+      		    </div>
+      		  </div>
+      		  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+      		    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      		    <span class="sr-only">Previous</span>
+      		  </a>
+      		  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+      		    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      		    <span class="sr-only">Next</span>
+      		  </a>
+      		</div>
+      		
+      	</div>
+      </div>
+      <div class="row footer mt-3">
+      	<div class="col-md-9">
+  			<div style="font-family: 'Rubik Mono One', sans-serif;font-size: .75rem;">
+  				High Country Hardscape & Landscape Designs<br/>	1101 Mariaville Rd, Schenectady, NY 12306<br/>
+  				<a href="tel:518-527-3964">518-527-3964</a>
+  			</div>
+  		</div>
+  		<div class="col-md-3 text-right">
+      		<a href="https://www.facebook.com/HighCountryHardscapes/" target="_blank" title="Visit our facebook page and like us to stay up to date"><i class="fab fa-facebook-square fa-2x"></i></a>
+      	</div>
+      </div>
+    </div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
